517b8c49b830f30d482b7c22130986b855f1278c
Steven Nguyen <snguyen155@student.hbuhsd.edu>
2024-09-03 11:19:10 -0700

    finished

--- problem3.java
+++ problem3.java
@@ -1,1 +1,11 @@
+public class problem3
+{
+  public static void main(String[] args)
+  {
+    System.out.println("=========");
+    System.out.println("= 0   0 =   Java is Awesome!");
+    System.out.println("=   v   =");
+    System.out.println("=========");
+  }
+}
 
